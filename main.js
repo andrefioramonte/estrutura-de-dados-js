6cc185eddd5e90b7029e263521798c6a6edff764
ls1w <michelle_j.b@hotmail.com>
2023-11-28 11:00:12 -0300

    projeto-finalizado

--- main.js
+++ main.js
@@ -3,43 +3,48 @@
 const users = [
     {
         nome: 'Ana Coelho' ,
-        id: '1325484564' ,
+        id: '1564' ,
         email: 'ana@gmail.com' ,
-        celular: '11993295505' ,
+        celular: '11 9-9969-5505' ,
         cidade: 'barueri',
-        image: './img/ana-coelho.jpg'
+        image: './img/ana-coelho.jpg',
+        status: true
     },
     {
         nome: 'Hugo Gomes' ,
-        id: '9625488564' ,
+        id: '9624' ,
         email: 'Hugo@gmail.com' ,
-        celular: '11363295505' ,
+        celular: '11 9-9969-5505',
         cidade: 'barueri',
-        image: './img/hugo-gomes.jpg'
+        image: './img/hugo-gomes.jpg',
+        status: false
     },
     {
         nome: 'Joana Pereira' ,
-        id: '6948488564' ,
+        id: '6564' ,
         email: 'Joana@gmail.com' ,
-        celular: '11996855505' ,
+        celular: '11 9-9969-5505' ,
         cidade: 'osasco',
-        image: './img/joana-pereira.jpg'
+        image: './img/joana-pereira.jpg',
+        status: true
     },
     {
         nome: 'Mariana Silva',
-        id: '6858488564',
+        id: '8564',
         email: 'mari@gmail.com',
-        celular: '11999695505',
+        celular: '11 9-9969-5505',
         cidade: 'itapevi',
-        image: './img/mariana-silva.jpg'
+        image: './img/mariana-silva.jpg',
+        status: true
     },
     {
         nome: 'José Chaves' ,
-        id: '98975488564' ,
+        id: '9897' ,
         email: 'jose@gmail.com' ,
-        celular: '11985295505' ,
+        celular: '11 9-9969-5505',
         cidade: 'cotia',
-        image: './img/jose-chaves.jpg'
+        image: './img/jose-chaves.jpg',
+        status: false
     }
 ]
 
@@ -48,10 +53,22 @@ const botaoExibir =document.getElementById('exibir')
 function criarCard (user) {
     const container = document.getElementById('data-container')
     const card = document.createElement('div')
-    card.className = 'card'
+
+    // outra forma  let status = user.status ? '' : 'sombra'
+
+    let status
+    if(user.status){
+        const status = ''
+    }else{
+        status = 'sombra'
+    }
+    
+    card.className = `card ${status}`
     card.innerHTML = `
         <img src="${user .image}" alt="${user .nome}" class="card__photo">
         <h3 class="card__name">${user .nome}</h3>
+        <p class="card__user-email">${user .email}</p>
+        <p class="card__user-celular">${user .celular}</p>
         <p class="card__user-id">${user .id}</p>
     `
 
